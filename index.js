d13d8958c1dc6250c04be57acf89f8310a3a800a
Adib Faqih <foezst@gmail.com>
2020-03-31 20:36:39 +0700

    solved, selection-sort

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function selectionSort(arr) {
-
+  for (var i = 0; i < arr.length; i++) {
+    var idx = i;
+    for (var j = i; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) {
+        idx = j;
+      }
+    }
+    var temp = arr[i];
+    arr[i] = arr[idx];
+    arr[idx] = temp;
+  }
+  return arr;
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
