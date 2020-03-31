91beb75ac5a216b111ce606d3af5ed167b52a956
vinita-dewi <61119004+vinita-dewi@users.noreply.github.com>
2020-03-31 22:24:09 +0700

    finished selection

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function selectionSort(arr) {
 
+    for (var j = 0; j < arr.length; j++) {
+        var start = arr[j]
+        var minNum = arr[j] 
+        var indexMin 
+        var counter = 0
+        debugger
+        for (var i = j + 1; i < arr.length; i++) {
+            if (arr[i] < minNum) {
+                minNum = arr[i]
+                indexMin = i
+                counter++
+            }
+        }
+        if( counter > 0 ){
+            arr[j] = minNum
+            arr[indexMin] = start
+        }
+        // debugger
+
+
+    }
+    return arr
+
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
