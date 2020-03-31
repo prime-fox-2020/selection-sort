c47de317e7dab9cfad39d455f01cd69f9282398a
rezarr-dev <rezarr.business@gmail.com>
2020-03-31 16:49:42 +0700

    selection sort done

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function selectionSort(arr) {
-
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr.length; j++){
+      if (arr[i] < arr[j]){
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+      }
+    }
+  }
+  return arr
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
