f489789f61702b4799ffe1dfad01ecaaac5b8d00
Mercen1 <nurhabib102@gmail.com>
2020-03-31 15:49:55 +0700

    edit index.js solve

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-function selectionSort(arr) {
 
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        var temp = arr[i]
+        var cek = false
+        for (let j  = i+1; j < arr.length; j++) {
+            if(temp>arr[j]){
+                temp=arr[j]
+                var index = j
+                cek = true
+            }
+        }
+        if(cek){
+            var tampung = arr[i]
+            arr[i]=temp
+            arr[index]=tampung
+        }
+    }
+
+    return arr
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
 // [ 2, 33, 52, 73, 106 ]
 
 console.log(selectionSort([13, 5, 22, 99, 11]));
-// [ 5, 11, 13, 22, 99 ]
+// // [ 5, 11, 13, 22, 99 ]
 
 // abaikan code dibawah
 module.exports = selectionSort;
