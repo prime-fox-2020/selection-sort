15d9b28c7bb3b7ba699853b5745d91a9ea28b66f
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-03-31 13:30:57 +0700

    sorted

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++){
+        let indexOfMin = i;
+        let j = i; 
+        let min = arr[i];
 
+        
+        while (j < arr.length){
+            if (arr[j] < min){
+                min = arr[j];
+                indexOfMin = j;
+            }
+            j++;
+        }
+
+        if (indexOfMin !== i){ 
+            let tmp = arr[i];
+            arr[i] = arr[indexOfMin];
+            arr[indexOfMin] = tmp;
+        }
+    }
+    return arr;
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
