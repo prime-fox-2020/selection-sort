15e00faaa4078fa1b6cf26a82e099e6c27141656
Xaber <getar.metal@gmail.com>
2020-03-31 22:10:51 +0700

    selection-sort first commit

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function selectionSort(arr) {
-
+    for (var a = 0; a < arr.length; ++a) {
+        var min = a;
+        for (var b = a; b < arr.length; ++b) {
+            if (arr[min] > arr[b]) {
+                min = b;
+            }
+        }
+        var sementara = arr[min];
+        arr[min] = arr[a];
+        arr[a] = sementara;
+    }
+    return arr;
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
