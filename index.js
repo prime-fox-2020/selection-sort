3199c6923fa796f0bf190c33e2fa6dccef0d90c4
rinotoharto <rinotoharto@gmail.com>
2020-03-31 18:32:20 +0700

    selection sort done

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let min = i
+        for(let j = i+1; j < arr.length; j++) {
+          if(arr[j] < arr[min]) {
+            min = j
+          }
+        }
 
+        let temp = arr[min]
+        arr[min] = arr[i]
+        arr[i] = temp
+    }
+    return arr
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
