4782b3c4af48dc388ed49ea7937f30a9d430040f
Sujackson <su_jackson99@yahoo.com>
2020-03-31 20:47:09 +0700

    done

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let kecil=i
+        for (let j = i+1 ; j < arr.length; j++) {
+            if(arr[kecil]>arr[j]){
+                kecil=j
+            }
+        }
+        if(kecil!==i){
+            var temp=arr[i]
+            arr[i]=arr[kecil]
+            arr[kecil]=temp
+        }   
+    }
+    return arr
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
@@ -10,3 +23,4 @@ console.log(selectionSort([13, 5, 22, 99, 11]));
 
 // abaikan code dibawah
 module.exports = selectionSort;
+
