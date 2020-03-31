59e343775cb7dec997fc309b78e5d452696dade6
susilo hartomo <encusilo545110@gmail.com>
2020-03-31 15:43:33 +0700

    release 0

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function selectionSort(arr) {
-
+    var hasil = []
+    while (arr.length != 0) {
+        let nilaiTerkecil = arr[0]
+        let indexNilaiTerkecil = 0
+        for (let j = 1; j < arr.length; j++) {
+            if (arr[j] < nilaiTerkecil) {
+                nilaiTerkecil = arr[j]
+                indexNilaiTerkecil = j
+            }
+        }
+        console.table([arr]);
+        console.log('nilaiTerkecil: ', nilaiTerkecil);
+        hasil.push(nilaiTerkecil)
+        arr.splice(indexNilaiTerkecil, 1)
+    }
+    return hasil
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
