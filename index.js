7917b1baa42ad98674118aa76c1c428508aeb606
syukur <syukurbaru@gmail.com>
2020-03-31 22:21:46 +0700

    add file selection sort

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function selectionSort(arr) {
 
+    for (var i = 0; i < arr.length; i++) {
+        var minValue = arr[i];
+        var indeks = i
+        // console.log(minValue);
+        for (var j = i + 1; j < arr.length; j++) {
+            // swap min value
+            if (arr[j] < minValue) {
+                minValue = arr[j]
+                indeks = [j]
+            }
+        }
+        arr[indeks] = arr[i]
+        arr[i] = minValue
+    }
+    return arr
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
