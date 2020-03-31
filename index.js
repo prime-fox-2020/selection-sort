395a7e85e3e838113a030a277df47163effe99e2
Samirudin242 <samtheyaku@gmail.com>
2020-03-31 16:16:43 +0700

    selection-sort done

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function selectionSort(arr) {
-
+    for(var i = 0; i < arr.length; i++) {
+        var temp = i;
+        for(var j = i+1; j < arr.length; j++) {
+            if(arr[temp] > arr[j]) {
+                temp = j;
+            }
+        }
+            [arr[i], arr[temp]] = [arr[temp], arr[i]]
+    }
+    return arr;
 }
 
-console.log(selectionSort([33, 2, 52, 106, 73]));
+
+console.log(selectionSort([33, 52, 2, 106, 73]));
 // [ 2, 33, 52, 73, 106 ]
 
 console.log(selectionSort([13, 5, 22, 99, 11]));
