8beff3a047181eb62d9b5f14f0d154eebbf1b579
SonyMa <sonymartha4@gmail.com>
2020-03-31 23:28:26 +0700

    first

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function selectionSort(arr) {
-
+    var length = arr.length
+    for (let i=1; i<length; i++) {
+         key = arr[i]
+         j = i-1
+        while (j>=0 && arr[j]>key) {
+            arr[j+1] = arr[j]
+            j -= 1
+        }
+        arr[j+1] = key
+    }
+    return arr
 }
+
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
 // [ 2, 33, 52, 73, 106 ]
