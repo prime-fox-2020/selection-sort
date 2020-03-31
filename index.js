f3db620aab7ae384a1d56973cb4faa1264cd0aa0
charles2901 <chrlsjnthn@gmail.com>
2020-03-31 13:58:42 +0700

    solve

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function selectionSort(arr) {
-
+    for(let i = 0; i < arr.length; i++){
+        let min = arr[i]
+        let index = i
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[j] < min){
+                min = arr[j]
+                index = [j]
+            }
+        }
+        arr[index] = arr[i]
+        arr[i] = min
+    }
+    return arr
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
