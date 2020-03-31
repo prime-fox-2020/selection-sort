d4e39d405d6c4002f11e5c65e957d07e66d01f4f
fahrulnoer <fahrulnoer17@gmail.com>
2020-03-31 15:35:06 +0700

    selection-sort

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let index = i
+        for (let j = i; j < arr.length; j++) {
+            if (arr[index] > arr[j]) {
+                index = j
+            }
+            // console.log(`j :${j}`,`i : ${i}`);
+        }
+        debugger
+        let temp = arr[i]
+        arr[i]= arr[index]
+        arr[index] = temp
+    }
+    return arr
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
