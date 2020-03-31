3ef9e5981951b2a2c2c3bb7f2b8da0c31b664a31
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-03-31 23:54:40 +0700

    selection sort done

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 function selectionSort(arr) {
+    let penanda = 0
+    for(let b = 0; b < arr.length; b++){
+        for(let a = 0; a <= arr.length; a++){
+            if(arr[a] < arr[penanda]){
+                let temp = arr[a]
+                arr[a] = arr[penanda]
+                arr[penanda] = temp
+                penanda++
 
+            }
+        
+        }
+        penanda++
+    }
+    return arr
 }
 
-console.log(selectionSort([33, 2, 52, 106, 73]));
+ console.log(selectionSort([33, 2, 52, 106, 73]));
 // [ 2, 33, 52, 73, 106 ]
 
 console.log(selectionSort([13, 5, 22, 99, 11]));
