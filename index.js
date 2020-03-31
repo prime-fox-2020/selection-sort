1151ea06f57cd6f5b772849a11f108d4ae6b575b
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-03-31 18:04:13 +0700

    tugas selection sort

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let simpan = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[simpan] > arr[j]) {
+                simpan = j;
+            }
+        }
+        if (simpan !== i) {
+            let temp = arr[i];
+            arr[i] = arr[simpan];
+            arr[simpan] = temp;
+        }
+    }
+    return arr;
 }
 
 console.log(selectionSort([33, 2, 52, 106, 73]));
